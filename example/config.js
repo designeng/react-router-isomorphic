e76b4ec36e9439df1c308a4079b66181504ae00e
Denis Savenok <denissavenok@gmail.com>
2016-01-14 12:57:29 +0400

    deleted extra files

--- example/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    socketio: {
-        host: "http://localhost"
-    }
-}
--- example/index.js
+++ example/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Navigation from './navigation'
-import Chat from './chat'
 import Users from './users'
 
 export default class Application extends React.Component {
@@ -9,7 +8,6 @@ export default class Application extends React.Component {
         return (
             <div id="app-wrapper">
                 <Navigation />
-                <Chat />
                 <Users />
             </div>
         )
