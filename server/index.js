592c05fff23246363e4b1e7ee8eee17270d39904
Denis Savenok <denissavenok@gmail.com>
2016-01-19 23:02:37 +0400

    removed extra files and deps

--- server/index.js
+++ server/index.js
@@ -2,36 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 var webpack = require('webpack');
-
-var FalcorServer = require('falcor-express');
-
-/* ---- import falcor routers ---- */
-import NavigationRouter     from './api/falcor/routers/navigation';
-import UserRouter           from './api/falcor/routers/user';
-
-var userRouter = require('./routers/user');
-var pagesRouter = require('./routers/pages');
-
-import chatFactory from './routers/chat';
-var chatRouter = chatFactory(io);
 
 var port = process.env.PORT || 8080;
 app.set('port', port);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-/* ---- falcor models API ---- */
-app.use('/navigation/model.json',   FalcorServer.dataSourceRoute(() => new NavigationRouter()));
-app.use('/user/model.json',         FalcorServer.dataSourceRoute(() => new UserRouter()));
-
-// express routers
-// page '/user'
-app.use('/', userRouter);
-app.use('/', chatRouter);
-app.use('/', pagesRouter);
 
 // static pages
 app.use(express.static('./public'));
